61a97eb6266db269c25ea405bdff271ae3829471
jsgilberto <alv.mtz94@gmail.com>
2018-05-30 14:54:53 +0200

    created my_select method

--- enumerable_methods.rb
+++ enumerable_methods.rb
@@ -10,13 +10,32 @@ module Enumerable
       yield(self[i], i)
     end
   end
+
+  def my_select
+    new_arr = []
+    self.my_each do |value|
+      if yield(value)
+        new_arr.push(value)
+      end
+    end
+    return new_arr
+  end
 end
 
-
+# Testing below
+puts "Testing my_each"
 [1,2,3].my_each do |x|
   puts x
 end
 
+puts "Testing my_each_with_index"
 [1,2,3].my_each_with_index do |value, i|
   puts "#{value} has index: #{i}."
 end
+
+puts "Testing my_select"
+evenNumbers = [1,2,3,4,5,6,7,8].my_select do |value|
+  value % 2 == 0
+end
+
+puts evenNumbers
