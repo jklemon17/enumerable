9fe60b547da4c42b322453cbf502955960f74cc0
jsgilberto <alv.mtz94@gmail.com>
2018-05-30 14:44:27 +0200

    created my_each_with_index

--- enumerable_methods.rb
+++ enumerable_methods.rb
@@ -5,10 +5,18 @@ module Enumerable
     end
   end
 
-  
+  def my_each_with_index
+    for i in 0...self.length do
+      yield(self[i], i)
+    end
+  end
 end
 
 
 [1,2,3].my_each do |x|
   puts x
+end
+
+[1,2,3].my_each_with_index do |value, i|
+  puts "#{value} has index: #{i}."
 end
